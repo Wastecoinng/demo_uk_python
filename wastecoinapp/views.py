c765c31a1101b0ed402702139686897adb328620
todak2000 <todak2000@gmail.com>
2020-07-14 17:34:04 +0100

    scan bug

--- wastecoinapp/views.py
+++ wastecoinapp/views.py
@@ -458,12 +458,12 @@ def scanner_camera(request):
 
     while True:
         success,img = cap.read()
-        for barcode in decode(img):
-            myData = barcode.data.decode('utf-8')
-            print(myData) 
-            pts = np.array([barcode.polygon], np.int32)
-            pts = pts.reshape((-1,1,2))
-            cv2.polylines(img,[pts], True (255,0,255), 5)
+        barcode = decode(img)
+        myData = barcode.data.decode('utf-8')
+        print(myData) 
+        pts = np.array([barcode.polygon], np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img,[pts], True, (255,0,255), 5)
         cv2.imshow('Result', img)
         cv2.waitKey(1)
   
